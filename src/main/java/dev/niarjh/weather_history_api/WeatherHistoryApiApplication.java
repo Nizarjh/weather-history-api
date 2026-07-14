6254d6cdb1e6956734b78ca4fe2bb488dbc672ea
Nizarjh <chel773@tutamail.com>
2026-07-15 02:32:05 +0400

    Initial commit

---
+++ src/main/java/dev/niarjh/weather_history_api/WeatherHistoryApiApplication.java
@@ -0,0 +1,13 @@
+package dev.niarjh.weather_history_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherHistoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherHistoryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/niarjh/weather_history_api/WeatherHistoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.niarjh.weather_history_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherHistoryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
