4356ca1b9083c8c671c7a0d2afe27415e42f7c22
Nizarjh <chel773@tutamail.com>
2026-07-16 09:03:46 +0400

    feat: implement CityService and added mapper

--- src/main/java/dev/niarjh/weather_history_api/mappers/CityMapper.java
+++ src/main/java/dev/niarjh/weather_history_api/mappers/CityMapper.java
@@ -1,8 +1,10 @@
 package dev.niarjh.weather_history_api.mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 
+import dev.niarjh.weather_history_api.client.dto.GeocodingResult;
 import dev.niarjh.weather_history_api.dto.GetCityResponseDto;
 import dev.niarjh.weather_history_api.models.City;
 
@@ -10,4 +12,9 @@ import dev.niarjh.weather_history_api.models.City;
 public interface CityMapper {
 
     GetCityResponseDto toGetCityResponseDto(City city);
+
+    @Mapping(target = "cityId", ignore = true)
+    @Mapping(target = "measurements", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    City toCityEntity(GeocodingResult geocoding);
 }
---
+++ src/main/java/dev/niarjh/weather_history_api/services/CityService.java
@@ -0,0 +1,71 @@
+package dev.niarjh.weather_history_api.services;
+
+import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import dev.niarjh.weather_history_api.client.GeoCodingClient;
+import dev.niarjh.weather_history_api.client.dto.GeocodingResponse;
+import dev.niarjh.weather_history_api.dto.AddCityRequestDto;
+import dev.niarjh.weather_history_api.dto.CitySearchFilter;
+import dev.niarjh.weather_history_api.dto.GetCityResponseDto;
+import dev.niarjh.weather_history_api.mappers.CityMapper;
+import dev.niarjh.weather_history_api.models.City;
+import dev.niarjh.weather_history_api.repositories.CityRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.AllArgsConstructor;
+
+@Service
+@AllArgsConstructor
+public class CityService {
+    private final CityRepository repository;
+    private final CityMapper cityMapper;
+    private final GeoCodingClient geoCodingClient;
+
+    public GetCityResponseDto getCityById(UUID id) {
+        City cityEntity = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Not found by id = " + id));
+        return cityMapper.toGetCityResponseDto(cityEntity);
+
+    }
+
+    public Page<GetCityResponseDto> getAllCities(CitySearchFilter filter) {
+        Pageable pageable = getPageable(filter);
+        return repository.findByFilter(
+                filter.countryCode(),
+                pageable).map(cityMapper::toGetCityResponseDto);
+    }
+
+    public void addNewCity(AddCityRequestDto addCityRequestDto) {
+        GeocodingResponse geocodingResponse = geoCodingClient.findCity(addCityRequestDto.cityName(),
+                addCityRequestDto.countryCode());
+        var results = geocodingResponse.results();
+
+        if (results == null || results.isEmpty()) {
+            throw new EntityNotFoundException("City not found");
+        }
+        City city = cityMapper.toCityEntity(results.getFirst());
+        repository.save(city);
+    }
+
+    public void deleteCityById(UUID id) {
+        City city = repository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Not found by id = " + id));
+        repository.delete(city);
+    }
+
+    private static Pageable getPageable(CitySearchFilter filter) {
+        int pageSize = filter.pageSize() != null
+                ? filter.pageSize()
+                : 15;
+        int pageNumber = filter.pageNumber() != null
+                ? filter.pageNumber()
+                : 0;
+
+        return Pageable
+                .ofSize(pageSize)
+                .withPage(pageNumber);
+    }
+}
